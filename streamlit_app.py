56793069be7cb256f49e90587c0841009ebb916a
JesusASantillanMinila <alosantillan@gmail.com>
2026-02-05 20:19:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import arviz as az
 from data_generation import generate_mmm_data
 from mmm_analysis import run_bayesian_mmm
 
